06d0b188944a260072618720c139e39a931353d8
Kirill Sapronov <sapronovkd@gmail.com>
2021-04-29 12:04:25 +0300

    Added reduce function

--- index.ts
+++ index.ts
@@ -4,3 +4,12 @@ export const groupBy = <Item, GroupByProperty extends string | number>(array: It
     (prev[groupKey] = prev[groupKey] || []).push(cur);
     return prev;
   }, {} as Record<GroupByProperty, Item[]>);
+
+
+export const reduce = <T, U extends number | Object>(array: T[], callbackfn: (acc: U, currentValue: T, currentIndex: number, array: T[]) => U, initialValue: U): U => {
+  let acc = <U>(initialValue === undefined ? 0 : initialValue)
+  for (let i=0;i<array.length;i++) {
+    acc = callbackfn(acc, array[i], i, array)
+  }
+  return acc
+}
